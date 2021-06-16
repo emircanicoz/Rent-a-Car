31aa544086b39a0201050312242192aea1b3e48b
emiricoz <tilauss0@gmail.com>
2021-06-16 17:09:33 +0300

    Final proje

---
+++ Internet_Programcılıgı/arackiralamaproject/Controllers/DefaultController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using arackiralamaproject.Models.Entity;
+
+
+namespace arackiralamaproject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        dbkiralikaracEntities1 db = new dbkiralikaracEntities1();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLCARS.ToList();
+            return View(degerler);
+        }
+        public ActionResult About()
+        {
+            
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Services()
+       
+        {
+            
+
+            return View();
+        }
+    }
+
+
+}
